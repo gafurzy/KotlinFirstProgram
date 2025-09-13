13390235d9027cb41f144cb9b21079abf3a2eee5
gafurzy <gafurzy64@gmail.com>
2025-09-13 15:12:27 +0700

    P. Boolean (true or false)

---
+++ src/P. Boolean (true or false).kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varBool: Boolean = true
+    if (varBool)
+        println("varBool bernilai " + varBool)
+    var varInt : Boolean = true
+    if (varInt)
+        println("varInt bernilai " + varInt)
+
+}
